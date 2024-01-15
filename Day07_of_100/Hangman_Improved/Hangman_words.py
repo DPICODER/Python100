f3550c21ee4de5b8b429c159769efe4a5a1eca3c
DPICODER <varunsaipannala@gmail.com>
2024-01-15 23:51:13 +0530

    Word list for Hangman

---
+++ Day07_of_100/Hangman_Improved/Hangman_words.py
@@ -0,0 +1,15 @@
+words = [
+    "rat",
+    "dog",
+    "cat",
+    "mamooth",
+    "monkey",
+    "elephant",
+    "tiger",
+    "lion",
+    "giraffe",
+    "zebra",
+    "capybara",
+    "groundhog",
+    "cockroach"
+]
