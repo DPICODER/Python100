bc6e5efda38be153a906e3fb7cf8fa95e2b94266
DPICODER <varunsaipannala@gmail.com>
2024-01-15 23:51:16 +0530

    Basics of dictionaries

---
+++ Day09_of_100/Dictionary_basics.py
@@ -0,0 +1,24 @@
+dictionary={
+    "AssassinsCreed":"A Open World Story Game",
+    "Call of Duty" : "A Real life Shooter Game",
+    "PUBG" : "A Survival Concept BattleRoyale Game"
+}
+# Retriving items from a dict
+print(dictionary["Call of Duty"])
+
+
+# Adding items to a Dict
+
+dictionary["Finals"] = "The Destruction Game"
+
+# print(dictionary)
+
+
+# Create a Empty Dictionary
+empty_dict = {}
+
+# wipe an existing Dict
+
+dictionary = {}
+
+print(dictionary)
