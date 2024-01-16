0f601ff4d4f88f48d47510e7077a6c7cda45f68c
DPICODER <varunsaipannala@gmail.com>
2024-01-17 00:51:37 +0530

    Calculator Part 2

--- Day10_of_100/Calculator.py
+++ Day10_of_100/Calculator.py
@@ -1,5 +1,5 @@
 # Calculator
-
+import os
 #Adding
 def add(n1,n2):
     return n1 + n2
@@ -24,15 +24,28 @@ operators = {
 }
 
 
+def Calculator():
+    """A Fuction build for simple calculation based on Recursion and function calls for 2 or More Numbers"""
+    os.system("cls")
+    to_continue = True
+    num1 = float(input("Enter a First Number ?:"))
+    for ops in operators:
+        print(ops)
 
-num1 = int(input("Enter a First Number ?:"))
-num2 = int(input("Enter a Second Number ?:"))
+    while to_continue:
+        symbol = input("Pick a symbol to perform Operation from Above : ")
+        num2 = float(input("Enter a Next Number ?:"))
+        Calc_func = operators[symbol]
+        inital_ans = Calc_func(num1,num2)
+        print(f"\n\n---->>{num1} {symbol} {num2} = {inital_ans}")
+        print("\nDo You Want to Continue ?")
+        decision = input("Enter .: \n 'y' -> to continue \n 'n' -> to perform new calculation \n 'x' -> to Exit\n:")
+        if decision=="y":
+            num1 = inital_ans
+        elif decision =="n":
+            Calculator()
+        else:
+            to_continue = False
 
-for ops in operators:
-    print(ops)
 
-symbol = input("Pick a symbol to perform Operation from Above : ")
-
-Calc_func = operators[symbol]
-
-print(f"The {num1} {symbol} {num2} = {Calc_func(num1,num2)}")
+Calculator()
