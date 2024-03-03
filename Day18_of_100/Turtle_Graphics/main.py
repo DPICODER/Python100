f8255a66a5bd581acb726cc601ee0e6ae74b623d
DPICODER <varunsaipannala@gmail.com>
2024-03-03 21:10:08 +0530

    Turtle

--- Day18_of_100/Turtle_Graphics/main.py
+++ Day18_of_100/Turtle_Graphics/main.py
@@ -1,10 +1,11 @@
 import random
+import turtle
 from turtle import Turtle,Screen
 
 Tt = Turtle()
 Tt.shape("classic")
 Tt.color("Black")
-
+turtle.colormode(255)
 
 def draw_square():
     for _ in range(4):
@@ -16,6 +17,12 @@ def draw_square():
 # draw_incr_square()
 
 
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return  random_color
 def draw_dashed_line():
     Tt.speed(1)  # Set the speed of the turtle
 
@@ -40,7 +47,7 @@ def draw_dashed_line():
 # draw_dashed_line()
 
 colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "pink", "cyan", "magenta"]
-
+directions = [0, 90, 180, 270]
 
 def draw_diff_shapes(no_of_sides):
 
@@ -53,11 +60,30 @@ def draw_diff_shapes(no_of_sides):
         no_of_sides += 1
 
 
-for shape_n in range(3,11):
-    Tt.color(random.choice(colors))
-    draw_diff_shapes(shape_n)
+# for shape_n in range(3,11):
+#     Tt.color(random.choice(colors))
+#     draw_diff_shapes(shape_n)
+def random_walk():
+    for _ in range(500):
+        Tt.pensize(8)
+        Tt.speed(0)
+        Tt.color(random_color())
+        Tt.forward(30)
+        Tt.setheading(random.choice(directions))
 
-# draw_diff_shapes()
+
+
+def draw_spiral(size_gap):
+    Tt.speed(0)
+    for _ in range (int(360/size_gap)):
+        Tt.circle(100)
+        Tt.color(random_color())
+        Tt.setheading(Tt.heading()+size_gap)
+
+
+draw_spiral(5)
+
+# random_walk()
 
 screen = Screen()
 screen.exitonclick()
